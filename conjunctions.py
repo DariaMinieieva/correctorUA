0a9beba00d8d88a1180a735bbf8fe1c6451c6c4b
Olha Liuba <olgaluba3@gmail.com>
2021-04-17 01:17:03 +0300

    Add conjunctions module.

---
+++ conjunctions.py
@@ -0,0 +1,18 @@
+"""This module works with conjunctions and looks for punctuation errors
+(if there is no comma before a conjunction)."""
+
+import re
+
+def check_conjunctions(sentence: str) -> str or None:
+    """Returns the message about a punctuation error
+    with conjunctions if there is one."""
+    sentence = sentence.lower()
+    conjunctions = set(['а', 'але', 'однак', 'проте', 'зате', 'хоч', 'хоча'])
+    errors = []
+    for word in conjunctions:
+        pattern = rf'.*[^,] {word} .*'
+        if re.fullmatch(pattern, sentence):
+            errors.append(f"Перед {word} повинна стояти кома.")
+    if errors:
+        return errors
+    return None
