567439ac7a02d989690e54170987571771fc944a
Daria Minieieva <darya.minieieva@ucu.edu.ua>
2021-04-17 11:44:31 +0300

    Add changes

--- together_hyphen.py
+++ together_hyphen.py
@@ -4,72 +4,81 @@ import re
 
 
 def write_together(sentence: str) -> list:
-    """
-    Returns the list of error messages if there are the mistakes
-    with words that need to be written together.
-    """
-    errors = []
+    """Returns the list of error messages if there are the mistakes
+    with words that need to be written together."""
+    errors = {}
     together_prefixes = {'абро', 'авіа', 'авто', 'агро', 'аеро', 'аква', 'алко', 'анти',
-                         'арт', 'астро', 'аудіо', 'біо', 'боди', 'веб', 'віце',
-                         'геліо', 'гео', 'гідро', 'гіпер',
-                         'дендро', 'екзо', 'еко', 'економ', 'екс', 'етно', 'євро',
-                         'зоо', 'ізо', 'кібер', 'контр',
-                         'мета', 'метео', 'моно', 'мото', 'нарко', 'нео', 'онко',
-                         'палео', 'пан', 'пара', 'поп',
-                         'прес', 'псевдо', 'соціо', 'теле', 'фіто', 'фолк', 'фольк',
-                         'фоно', 'архі', 'архи', 'бліц',
-                         'гіпер', 'екстра', 'макро', 'максі', 'міді', 'мікро',
-                         'міні', 'мульти', 'нано', 'полі',
+                         'арт', 'астро', 'аудіо', 'біо', 'боди', 'веб', 'віце', 'геліо', 'гео', 'гідро', 'гіпер',
+                         'дендро', 'екзо', 'еко', 'економ', 'екс', 'етно', 'євро', 'зоо', 'ізо', 'кібер', 'контр',
+                         'мета', 'метео', 'моно', 'мото', 'нарко', 'нео', 'онко', 'палео', 'пан', 'пара', 'поп',
+                         'прес', 'псевдо', 'соціо', 'теле', 'фіто', 'фолк', 'фольк', 'фоно', 'архі', 'архи', 'бліц',
+                         'гіпер', 'екстра', 'макро', 'максі', 'міді', 'мікро', 'міні', 'мульти', 'нано', 'полі',
                          'преміум', 'супер', 'топ', 'ультра', 'флеш', 'напів', 'аби', 'ані', 'де'}
     for prefix in together_prefixes:
-        pattern = rf'^(.* )*{prefix}[^\w].*'
-        if re.fullmatch(pattern, sentence):
-            errors.append(f'Слова з префіксом "{prefix}" пишуться разом.')
+        if prefix in sentence:
+            pattern = re.findall(
+                rf'(?<=[^\w-]){prefix}[-\s][\w]*(?=[^\w-])', sentence)
+            pattern += re.findall(rf'^{prefix}[-\s][\w]*(?=[^\w-])', sentence)
+            pattern += re.findall(rf'(?<=[^\w-]){prefix}[-\s][\w]*$', sentence)
+            for key in pattern:
+                errors[key] = re.sub(rf'{prefix}.', prefix, key)
     return errors
 
 
 def specific_hyphen(sentence: str) -> list:
-    """
-    Returns the list of error messages if there are the mistakes
-    with words that need to be written with a hyphen due to specific rule.
-    """
-    errors = []
+    """Returns the list of error messages if there are the mistakes
+    with words that need to be written with a hyphen due to specific rule."""
+    errors = {}
     prefix = "по"
-    sufixes = ("ому", "и")
-    pattern = rf'(.* )*{prefix}[^-]([^ ]+)?({sufixes[0]}|{sufixes[1]}).*'
-    if re.fullmatch(pattern, sentence):
-        errors.append(f'Слова з префіксом "{prefix}" й суфіксами "{sufixes[0]}" та \
-"{sufixes[1]}" пишуться через дефіс.')
+    sufixes = "ому"
+    if 'по' in sentence and 'ому' in sentence:
+        pattern = re.findall(
+            rf'(?<=[^\w-]){prefix}[^-][\w]*{sufixes}(?=[^\w-])', sentence)
+        pattern += re.findall(
+            rf'^{prefix}[^-][\w]*{sufixes}(?=[^\w-])', sentence)
+        pattern += re.findall(
+            rf'(?<=[^\w-]){prefix}[^-][\w]*{sufixes}$', sentence)
+        for key in pattern:
+            errors[key] = re.sub(r'по\s?', 'по-', key)
     return errors
 
 
 def write_with_hyphen(sentence: str) -> list:
-    """
-    Returns the list of error messages if there are the mistakes
-    with words that need to be written with a hyphen.
-    """
-    errors = []
-    hyphen_prefixes = {'альфа', 'бета', 'гамма', 'дельта', 'казна', 'хтозна', 'бозна'}
+    """Returns the list of error messages if there are the mistakes
+    with words that need to be written with a hyphen."""
+    errors = {}
+    hyphen_prefixes = {'альфа', 'бета', 'гамма',
+                       'дельта', 'казна', 'хтозна', 'бозна'}
     hyphen_sufixes = {'таки', 'небудь'}
     for prefix in hyphen_prefixes:
-        prefix_pattern = rf'^(.* )*{prefix}[^-].*'
-        if re.fullmatch(prefix_pattern, sentence):
-            errors.append(f'Слова з префіксом "{prefix}" пишуться через дефіс.')
+        if prefix in sentence:
+            pattern = re.findall(
+                rf'(?<=[^\w-]){prefix}[^-][\w]*(?=[^\w-])', sentence)
+            pattern += re.findall(rf'^{prefix}[^-][\w]*(?=[^\w-])', sentence)
+            pattern += re.findall(rf'(?<=[^\w-]){prefix}[^-][\w]*$', sentence)
+            for key in pattern:
+                errors[key] = re.sub(r'{prefix}\s?', rf'{prefix}-', key)
+
     for sufix in hyphen_sufixes:
-        sufix_pattern = rf'.*[^-]{sufix}( .*)?'
-        if re.fullmatch(sufix_pattern, sentence):
-            errors.append(f'Слова з суфіксом "{sufix}" пишуться через дефіс.')
+        if sufix in sentence:
+            pattern = re.findall(rf'^\w*[^-]{sufix}(?=[^\w-])', sentence)
+            pattern += re.findall(
+                rf'(?<=[^\w-])\w*[^-]{sufix}(?=[^\w-])', sentence)
+            pattern += re.findall(rf'(?<=[^\w-])\w*[^-]{sufix}$', sentence)
+            for key in pattern:
+                errors[key] = re.sub(rf'\s?{sufix}', rf'-{sufix}', key)
     return errors
 
 
 def main_check(sentence: str) -> list or None:
-    """
-    Returns the list of error messages if there are the mistakes
-    with words that need to be written with a hyphen or together.
-    """
+    """Returns the list of error messages if there are the mistakes
+    with words that need to be written with a hyphen or together."""
     sentence = sentence.lower()
     together_errors = write_together(sentence)
     hyphen_errors = write_with_hyphen(sentence)
     specific_hyphen_errors = specific_hyphen(sentence)
-    errors = together_errors + hyphen_errors + specific_hyphen_errors
+    errors = {}
+    errors.update(together_errors)
+    errors.update(hyphen_errors)
+    errors.update(specific_hyphen_errors)
     return errors if errors else None
