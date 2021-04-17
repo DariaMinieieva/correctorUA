0b82d6455841ca835c31ab21f90eb28617eefe40
Daria Minieieva <darya.minieieva@ucu.edu.ua>
2021-04-17 05:13:37 +0300

    Add bot 2.0

--- main_telegram.py
+++ main_telegram.py
@@ -1,20 +1,73 @@
+"""
+This module contains fucntions that work
+directly with a telegram bot
+"""
+
 import telebot
-from main_logic import check_for_mistake
+from main_logic import check_for_mistake, check_conjunctions, correct_msg, check_lang, main_check
 
 bot = telebot.TeleBot('token')
 
 
 @bot.message_handler(commands=['start'])
 def handle_command(message):
-    bot.reply_to(message, "Hello, welcome to Telegram Bot")
+    '''
+    Initial message
+    '''
+    bot.reply_to(message, "Привіт! Я бот - українізатор!")
 
 
 @bot.message_handler(func=lambda message: True)
 def handle_all_message(message):
+    '''
+    Send messages to the bot
+    '''
+    special_word(message, message.text)
     mess = message.text
     correct = check_for_mistake(mess)
-    for i in correct:
-        bot.reply_to(message, i)
+    correctes_sentence = correct_msg(mess)
+    errors = check_conjunctions(correctes_sentence)
+    translations = check_lang(correctes_sentence)
+    hyphen = main_check(correctes_sentence)
+
+    if correct:
+        for i in correct:
+            bot.reply_to(message, i)
+
+    if errors:
+        for i in errors:
+            bot.reply_to(message, i)
+
+    if translations:
+        for i in translations:
+            bot.reply_to(message, i)
+
+    if hyphen:
+        for i in hyphen:
+            bot.reply_to(message, i)
+
+
+def special_word(message, text):
+    '''
+    Special cases
+    '''
+    if "україн" in text:
+        if "українізатор" in text:
+            bot.reply_to(message, "Це я!")
+        else:
+            bot.reply_to(message, "Україна - лише з великої.")
+
+    if ("Тернопіль" in text) or ("тернопіль" in text):
+        bot.reply_to(message, "Файне місто")
+
+    if "сумно" in text:
+        bot.reply_to(message, "не сумуй!")
+
+    if ("важко" in text) or ("тяжко" in text):
+        bot.reply_to(message, "нічого, буде ще важче.")
+
+    if "БА" in text:
+        bot.reply_to(message, "ні, КН")
 
 
 bot.polling()
